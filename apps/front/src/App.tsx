ff12c72ecc101627c489d2bd48ef0c267167fc77
WittyPsyduck <wittypsyduck@outlook.com>
2023-03-06 22:52:34 +0800

    mermaid added to frontend

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import mermaid from 'mermaid'
 
-function App() {
-  const [count, setCount] = useState(0)
+const mermaidText = `
+graph TD 
+A[Client] --> B[Load Balancer] 
+B --> C[Server01] 
+B --> D[Server02]
+`
+
+const App = () => {
+  useEffect(() => {
+    mermaid.initialize({ startOnLoad: true })
+  }, [])
 
   return (
     <div className='App'>
       <div>
         <a href='https://reactjs.org' target='_blank' />
+        <pre className='mermaid'>{mermaidText}</pre>
       </div>
     </div>
   )
